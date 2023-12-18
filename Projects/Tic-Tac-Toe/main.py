99e212c1ec12363b7e9b6a52e1f6a8d2ab025c13
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2023-12-18 12:56:35 +0000

    Finish Tic Tac Toe

--- Projects/Tic-Tac-Toe/main.py
+++ Projects/Tic-Tac-Toe/main.py
@@ -1,30 +1,102 @@
-import tkinter as tk
-from tkinter import messagebox
+import os
 
-# 2. Create the board
-def create_board():
-    for x in range(3):
-        for y in range(3):
-            #Create buttons
-            button = tk.Button(window,
-                               text="", 
-                               font=("Arial", 50),
-                               height=2,
-                               width=6,
-                               bg="ligthblue",
-                               command=lambda row=x, col=y: handle_click(row, col))
-            button.grid(row=x, 
-                        column=y, 
-                        sticky="nsew")
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_win(turn: str, board: dict) -> None:
+    clear_screen()
+    print_board(board)
+    print("\n      Game Over \n")
+    print(f" **** Player {turn} has won ! \n\n")
+
+def print_board(board):
+    print()
+    print ("\t" + board['7'] + '|' +board['8'] + '|' + board['9'])
+    print ("\t" + "-+-+-")
+    print ("\t" + board['4'] + '|' +board['5'] + '|' + board['6'])
+    print ("\t" + "-+-+-")
+    print ("\t" + board['1'] + '|' +board['2'] + '|' + board['3'])
+    print()
+    
+
+def check_win(turn: str, board: dict) -> bool:
+    # Check rows
+    for row in range(3):
+        if board[f'{row * 3 + 1}'] == board[f'{row * 3 + 2}'] == board[f'{row * 3 + 3}'] == turn and board[f'{row * 3 + 1}'] != ' ':
+            print_win(turn, board)
+            return True
+
+    # Check columns
+    for col in range(3):
+        if board[f'{col + 1}'] == board[f'{col + 4}'] == board[f'{col + 7}'] == turn and board[f'{col + 1}'] != ' ':
+            print_win(turn, board)
+            return True
+
+    # Check diagonals
+    if (board['1'] == board['5'] == board['9'] == turn and board['1'] != ' ') or \
+       (board['3'] == board['5'] == board['7'] == turn and board['3'] != ' '):
+        print_win(turn, board)
+        return True
+
+    return False
+    
+
+def is_move_valid(move: str, board: dict) -> bool:
+    return move in board and board[move] == ' '
 
 
+def get_valid_move(board: dict) -> str:
+    while True:
+        move = input("Where do you want to go? : ")
+        if is_move_valid(move, board):
+            return move
+        else:
+            print("Invalid move. Please try again.")
+
+
+def prompt_restart():
+    return input("Do you want to play again? (y)es or (n)o: ").lower() == "y"
 
 def main():
-    # 1. Create Window
-    window = tk.Tk()
-    window.title("Tic Tac Toe")
-    
-    create_board()
+    while True:
+        the_board = {
+            '7': ' ', '8': ' ', '9': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '1': ' ', '2': ' ', '3': ' ',
+        }
+
+        board_keys = list(the_board.keys())
+
+        turn = "X"
+        count = 0
+
+        for _ in range(9):  # Change to 9 to match the number of moves
+            clear_screen()
+            print_board(the_board)
+            print(f"It's Player {turn}'s turn")
+
+            move = get_valid_move(the_board)
+
+            the_board[move] = turn
+            count += 1
+
+            if count >= 5:
+                win = check_win(turn, the_board)
+                if win:
+                    print_win(turn, the_board)
+                    break  # Exit the loop on win
+
+            if count == 9:
+                print("Game Over !")
+                print("It's a tie")
+                break  # Exit the loop on tie
+
+            turn = "O" if turn == "X" else "X"
+
+            #input("Press Enter to continue...")  # Pause until Enter is pressed
+
+        if not prompt_restart():
+            break  # Exit the outer loop on 'n' or invalid input
 
 if __name__ == "__main__":
     main()
---
+++ Projects/byko/test.py
@@ -0,0 +1,16 @@
+import csv
+
+# Specify the path to your CSV file
+csv_file_path = 'Projects/byko/csvfile.csv'
+
+# Open the CSV file
+with open(csv_file_path, 'r', newline='', encoding='utf-8') as csv_file:
+    # Create a CSV reader object
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # Read the CSV data into a list of lists
+    data = [row for row in csv_reader]
+
+# Print the data
+for row in data:
+    print(row)
