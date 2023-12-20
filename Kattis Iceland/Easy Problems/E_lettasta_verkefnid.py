4a25f3a6701b380c399589775e6f944be9a8119e
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2023-12-20 17:56:50 +0000

    more

--- Kattis Iceland/Easy Problems/E_lettasta_verkefnid.py
+++ Kattis Iceland/Easy Problems/E_lettasta_verkefnid.py
@@ -1,16 +1,20 @@
-n, m = map(int, input().split())
+# Read the input data
+num_problems = int(input())
+num_teams = int(input())
 problem_names = input().split()
+    
+# Initialize points for each problem
+points_for_problem = [0] * num_problems
 
-max_points = 0
-max_problem = ""
-
-
-for i in range(m):
+# Read points for each team
+for _ in range(num_teams):
     points = list(map(int, input().split()))
-    total_points = sum(points)
+    for i in range(num_problems):
+        points_for_problem[i] += points[i]
 
-    if total_points > max_points:
-        max_points = total_points
-        max_problem = problem_names[points.index(max(points))]
+# Find the problem with the maximum total points
+max_points = max(points_for_problem)
+max_problem = problem_names[points_for_problem.index(max_points)]
 
+# Print the name of the problem with the highest total points
 print(max_problem)
